38d45cb7f4558f4cf2641b41fb83ec8d73ceab92
John Tillett <tillett1957@gmail.com>
2016-01-07 17:42:15 +1100

    first commit of working 2015 files - onto 2016!!

---
+++ account_html_string.py
@@ -0,0 +1,50 @@
+# accont as a string to be printed by main function
+
+account_html_string = """
+
+<html moznomarginboxes mozdisallowselectionprint>
+
+<head> 
+<title>JT account test </title>
+</head>
+
+<body>
+
+<h4>  Account for Anaesthetic </h4>
+
+<h4>Dr John Tillett</h4>
+7 Henry Lawson Drive <br>
+Villawood NSW 2163 <br>
+<br>
+Phone 0408116320 <br>
+<br>
+<h4>Provider Number   0307195H</h4> <br>
+<br>
+Place of Service: <br> nowhere
+Diagnostic Endoscopy Centre <br>
+Suite 601,Level 6 <br>
+St Vincent's Clinic <br>
+Darlinghurst NSW 2010 <br>
+<br>
+Date of Service:   13-19-0987<br>
+<br>
+Patient and Fund Details: <br>
+
+<br>
+<br>ghjgjhgjh
+<br>
+
+
+
+Item Number     89809890<br>
+<br>
+<br>
+Total Fee <br>
+<br>
+Doctor who performed procedure:  blogggsa9<br>
+
+
+
+
+</html>
+"""
