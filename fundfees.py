521f068a7006f6f8d76c6ad907c243e710da3dbe
John Tillett <tillett1957@gmail.com>
2016-01-11 16:38:25 +1100

    clean up file structure of repo

--- fundfees.py
+++
@@ -1,9 +0,0 @@
-# a dictionary of health fund fees. Key is the fund acronym and the 
-#value is a list of  two numbers-  the first being the consult fee and the second the unit fee 
-
-
-# # 2015  fund_fees = {'HCF':[95.00,34.70],'BUPA':[73.00,31.10],'MPL':[73.00,32.70],'AHSA':[67.80,34.30],'AHM':[71.05,32.70],'NIB':[65.95,31.50],'AMA':[150.00,77.00],'GARRISON':[73.00,32.70],'VA':[50.00,50.00],'BB':[50.00,50.00],'OS':[75.00,30.00]}
-
-
-fund_fees = {'HCF':[90.30,34.70],'BUPA':[73.00,31.10],'MPL':[73.00,32.70],'AHSA':[68.40,34.70],'AHM':[71.05,32.70],'NIB':[65.95,31.50],'AMA':[150.00,77.00],'GARRISON':[73.00,32.70],'VA':[50.00,50.00],'BB':[50.00,50.00],'OS':[75.00,30.00]}
-
