956fba610a9329d63289397d653bb8b5885ee4dd
John Tillett <tillett1957@gmail.com>
2017-10-31 16:15:14 +1100

    rearrange file structure

---
+++ names_and_codes.py
@@ -0,0 +1,156 @@
+# names_and_codes.py
+
+NURSES_DIC = {'no': 'Nobue Chashin',
+              'ja': 'Jacqueline James',
+              'jg': 'Jacinta Goldenberg',
+              'js': 'Jacqueline Smith',
+              'yi': 'Yi Lu',
+              'pa': 'Parastoo Tavakoli Siahkali',
+              'la': 'Larissa Berman',
+              'ma': 'Mary Halter',
+              'su': 'Subeia Aziz Silva',
+              'be': 'Belinda Plunkett',
+              'ch': 'Cheryl Guise',
+              'ka': 'Katherine Turner', }
+
+ANAESTHETISTS = {'tt': 'Dr T Thompson',
+                 'sv': 'Dr S Vuong',
+                 'cb': 'Dr C Brown',
+                 'jr': 'Dr J Riley',
+                 'js': 'Dr J Stevens',
+                 'db': 'Dr D Bowring',
+                 'gos': "Dr G O'Sullivan",
+                 'jt': 'Dr J Tester',
+                 'rw': 'Dr Rebecca Wood',
+                 'mm': 'Dr M Moyle',
+                 'mon': "Dr Martine O'Neill",
+                 'ni': 'Dr N Ignatenko',
+                 'tr': 'Dr Timothy Robertson',
+                 'ms': 'Dr M Stone',
+                 'fd': 'Dr Felicity Doherty',
+                 'bm': 'Dr B Manasiev',
+                 'eoh': "Dr E O'Hare",
+                 'locum': 'locum',
+                 'jrt': 'Dr J Tillett'}
+
+REGULAR_ANAESTHETISTS = {'Dr J Tillett', "Dr G O'Sullivan", 'Dr T Thompson',
+                         'Dr S Vuong', 'Dr C Brown', 'Dr J Riley',
+                         'Dr J Stevens', 'Dr D Bowring', 'Dr J Tester'}
+
+VMOS = {'DR M Danta', 'DR R Gett', 'Prof R Lord', 'Dr G Owen', 'Dr A Meagher'}
+
+
+DOC_LIST = ['Dr C Bariol', 'DR M Danta', 'Dr R Feller', 'DR R Gett',
+            'Dr S Ghaly', 'Prof R Lord', 'Dr A Meagher',
+            'Dr A Stoita', 'Dr C Vickers', 'Dr S Vivekanandarajah',
+            'Dr A Wettstein', 'Dr D Williams']
+
+DOC_DIC = {'cb': 'Dr C Bariol',
+           'md': 'DR M Danta',
+           'rf': 'Dr R Feller',
+           'rg': 'DR R Gett',
+           'sg': 'Dr S Ghaly',
+           'rl': 'Prof R Lord',
+           'am': 'Dr A Meagher',
+           'as': 'Dr A Stoita',
+           'cv': 'Dr C Vickers',
+           'sv': 'Dr S Vivekanandarajah',
+           'aw': 'Dr A Wettstein',
+           'dw': 'Dr D Williams',
+           'go': 'Dr G Owen',
+           'wb': 'Dr W Bye',
+           'bb': 'Dr W Bye',
+           'vn': 'Dr V Nguyen',
+           'jm': 'Dr J Mill',
+           'cw': 'Dr Yang Wu',
+           'ak': 'Dr A Kim',
+           'ndl': 'Dr N De Luca'}
+
+LOCUMS = {'Dr W Bye',
+          'Dr V Nguyen',
+          'Dr J Mill',
+          'Dr Yang Wu',
+          'Dr A Kim'}
+
+PARTNERS = {'Dr C Bariol',
+            'Dr R Feller',
+            'Dr S Ghaly',
+            'Dr A Stoita',
+            'Dr C Vickers',
+            'Dr S Vivekanandarajah',
+            'Dr A Wettstein',
+            'Dr D Williams'}
+
+BANDERS = {'DR R Gett', 'Dr A Meagher', 'Dr A Wettstein', 'Dr G Owen'}
+
+CONSULTERS = {'Dr S Ghaly', 'Dr S Vivekanandarajah', 'Dr A Wettstein'}
+
+ASA_DIC = {'1': '92515-19',
+           '2': '92515-29',
+           '3': '92515-39',
+           '4': '92515-49'}
+
+UPPER_DIC = {'0': 'none',
+             'pe': '30473-00',
+             'pb': '30473-01',
+             'od': '41819-00',
+             'pa': '30478-20',
+             'halo': '30478-20',
+             'pp': '30478-04',
+             'pv': '30476-02',
+             'bravo': '30490-00'}
+
+COLON_DIC = {'0': 'none',
+             'co': '32090-00',
+             'cs': '32090-00',
+             'cb': '32090-01',
+             'cp': '32093-00',
+             'csp': '32093-00',
+             'sc': '32084-00',
+             'sb': '32084-01',
+             'sp': '32087-00'}
+
+BANDING_DIC = {'0': 'none',
+               'b': '32135-00',
+               'a': '32153-00'}
+
+FUND_ABREVIATION = {'h': 'hcf',
+                    'b': 'bup',
+                    'm': 'mpl',
+                    'n': 'nib',
+                    'a': 'ahsa',
+                    'd': 'ama',
+                    't': 'ahm',
+                    'bb': 'bb',
+                    'v': 'va',
+                    'o': 'os',
+                    'g': 'ga'}
+
+FUND_DIC = {'hcf': 'HCF',
+            'mpl': 'Medibank Private',
+            'bup': 'BUPA',
+            'nib': 'NIB',
+            'ama': "The Doctor's Fund",
+            'ahm': 'Australian Health Management',
+            'bb': 'Bulk Bill',
+            'va': 'Veterans Affairs',
+            'os': 'Overseas',
+            'ga': 'Garrison Health'}
+
+AHSA_DIC = {'te': 'Teachers Federation Health',
+            'we': 'Westfund',
+            'au': 'Australian Unity Health',
+            'cb': 'CBHS Health',
+            'de': 'Defence Health',
+            'cu': 'CUA',
+            'fr': 'Frank Health',
+            'gm': 'GMHBA',
+            'pe': 'Peoplecare Health',
+            'rt': 'Railway & Transport Health',
+            'gu': 'Grand United Corporate Health',
+            'he': 'health.com.au',
+            'hi': 'Health Insurance Fund',
+            'na': 'Naval Health Benefits',
+            're': 'Reserve Bank',
+            'o': 'other'}
+
